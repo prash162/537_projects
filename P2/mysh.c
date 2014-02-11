5603a36fe39f191af4b2a4ce73b77c5e8099c622
prash162 <chada2@wisc.edu>
2014-02-10 20:13:38 -0500

    multiple instr

--- P2/mysh.c
+++ P2/mysh.c
@@ -5,15 +5,17 @@
 #include<sys/types.h>
 #include<sys/wait.h>
 
-void error()      //error function
+void printerror()      //error function
 {
 char error_message[30] = "An error has occurred\n";
-write(STDERR_FILENO, error_message, strlen(error_message));
+int n=write(STDERR_FILENO, error_message, strlen(error_message));
+if(n==-1)
+{
+printerror();
+}
 }
 
-
-
-void pwd()
+void pwd()   //pwd func
 {
 char *path;
 path=(char*) malloc(256);
@@ -26,7 +28,7 @@ void cd(char* argv[])
 char *env = (char*)malloc(256);
   if (env==NULL)
   {
-  error();
+  printerror();
   }
 
 if(argv[1]==NULL)
@@ -34,9 +36,7 @@ if(argv[1]==NULL)
 
 else
 {
-
   strcpy(env,argv[1]);
- // strncpy(env,argv[1],(strlen(argv[1])-1));
 }
 
 int w;
@@ -44,7 +44,7 @@ w=chdir(env);
 
 if(w==-1)
   {
-  error();
+  printerror();
   }
 }
 
@@ -54,17 +54,14 @@ if(w==-1)
 int main(int argc , char *argv[])   // main
 {
 
-
-
 while(1)
 {
-  
    
  printf("537sh> "); 
   char* parse;
   parse=(char*)malloc(512*(sizeof(char)));
   if (parse==NULL)
-  {error();}
+  {printerror();}
    
 parse=fgets(parse,512,stdin);  
   
@@ -76,86 +73,149 @@ char * tok;
 int count=0;
 
 
+
 int k;
 for(k=0;(k<(strlen(parse)+1));k++)                            // To find number of arguments in the string
-{
-  if((((strncmp(p," ",1)))!=0) && ((strncmp(q," ",1))==0) )
+{  
+  if ((strchr(parse,';')!=NULL))
+  {
+  if((((strncmp(p,";",1)))==0) && ((strncmp(q,";",1))!=0) )
    { 
    count++;
    }
     p++;
     q++;
+  }
+
+
+  if ((strchr(parse,'+')!=NULL))
+  {
+  if((((strncmp(p,"+",1)))==0) && ((strncmp(q,"+",1))!=0) )
+  {
+  count++;
+  }
+    p++;
+    q++;
+  }
 }
+
+  
 count++;
- 
+
+
 char* argv[8];
 
 
-char * parse2=malloc(512);
-parse2=strncpy(parse2,parse,(strlen(parse)-1));
+char * parse2[count];
+char * parse3=malloc(512);
+//parse2[nm]=strncpy(parse2[nm],parse,(strlen(parse)-1));
+//int nm=0;
+//while(nm<=count)
 
-if((strchr(parse2,';')==NULL ) || (strchr(parse2,'+')==NULL ))
+
+parse3=strncpy(parse3,parse,(strlen(parse)-1));
+
+//  int nm=0;
+if ((strchr(parse3,';')!=NULL))
+{  
+  int nm=0;
+  tok=strtok(parse3,";");
+
+    while(tok!=NULL)
+    {
+    parse2[nm]=tok;
+    tok=strtok(NULL,";");
+    nm++;
+    }
+}
+
+else if((strchr(parse3,'+')!=NULL))
+{
+
+ int nm=0;
+ // int nm=0;
+  tok=strtok(parse3,"+");
+    while(tok!=NULL)
+    {
+    parse2[nm]=tok;
+    tok=strtok(NULL,"+");
+    nm++;
+    }
+}
+
+else
+{  parse2[0]=parse3;  }
+
+//printf("%d\n",count);
+
+int nm=0; 
+//while lop required here
+while(nm<=(count-1) )
+{
+
+  printf("========\n");
 {
   int l=0;
-  tok=strtok(parse2," ");   // Actual parsing to find arguments
-
+  tok=strtok(parse2[nm]," ");   // Actual parsing to find arguments
   while(tok!=NULL)    
   {
-   // printf("%s\n",tok);
   argv[l]=strdup(tok);
   tok=strtok(NULL," ");
   l++;
     }
   argv[l]=NULL;
-} 
+}
+
+
   
   int id;      
-
-   
-         //   id =fork();
-
-  
+ 
    if(strncmp(argv[0],"quit",4) ==0)   // issue with quit have to give it twice sometimes
        {
-
           exit(0);      
         }
 
-   else 
+   else if(strncmp(argv[0],"pwd",3)==0)  
+        {
+        pwd();
+        }
+
+  else if (strncmp(argv[0],"cd",2)==0)
+          {
+          cd(argv);          
+          }
+         
+else 
        {      
          id=fork();
               if(id==0)//child
-                    {         
-                            
-                         if(strncmp(argv[0],"pwd",3)==0)
-                             {
-                                   pwd();
-                              }
-     
-                          else if(strncmp(argv[0],"cd",2)==0)
-                             {
-                                cd(argv);
-                             }
- 
-                             else
-                                {   
-                              execvp(argv[0],argv);
-                                 error();
-                                }
-                     } 
+                    {   
+                      
+                     execvp(argv[0],argv);
+                     printerror();
+                    }
+        
 
                 else if(id>0) //parent
                    {
                   
                         pid_t status;
+                      
                     while(waitpid(0,&status,0)!=id);
-                    
-               //    while( waitpid(0, &status, WEXITED)!=id);
+                      nm++;        
+                    continue;
                    }
                else
-                 {  error();}
+                 { 
+                   printerror();
+                    break; 
+                 }
 
-         }           
+        }
+    
+  }
+
+  printf("========\n");
 }
 return 0;
 }
